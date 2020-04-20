3c6cf93907230770d9f60d37b5d2300259ae971a
Pavel Kohal <pavelkohal@gmail.com>
2020-04-20 10:17:59 +0300

    increase waiting time for expected conditions

--- automation-framework/src/test/java/page/AbstractPage.java
+++ automation-framework/src/test/java/page/AbstractPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 public class AbstractPage {
 
     protected WebDriver driver;
-    protected final int WAIT_TIMEOUT_SECONDS = 20;
+    protected final int WAIT_TIMEOUT_SECONDS = 60;
 
     protected AbstractPage(WebDriver driver)
     {
